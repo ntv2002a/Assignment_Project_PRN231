832c81e8394866188f0fbbe16a367ab24c745b5d
Nguyen Trung Vu <trungvu16502@gmail.com>
2024-03-22 04:12:02 +0700

    Update header and lesson info

--- Code_Assignment_Project/OnlineLearning_API/Controllers/LessonController.cs
+++ Code_Assignment_Project/OnlineLearning_API/Controllers/LessonController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OData.Query;
+using OnlineLearning_API.DTO.Lesson;
 using OnlineLearning_API.Models;
 
 namespace OnlineLearning_API.Controllers
@@ -20,6 +21,33 @@ namespace OnlineLearning_API.Controllers
             var lessonList = _context.Lessons.ToList();
 
             return Ok(lessonList.AsQueryable());
+        }
+
+        [EnableQuery]
+        [HttpGet("LessonInfo")]
+        //[Authorize(Policy = "student")]
+        public IActionResult lessonInfo()
+        {
+            var lessonListRaw = _context.Lessons.ToList();
+            var lessonList = new List<LessonDTO>();
+
+            foreach (var item in lessonListRaw)
+            {
+                lessonList.Add(new LessonDTO
+                {
+                    LessonId = item.LessonId,
+                    LessonName = item.LessonName,
+                    LessonContent = item.LessonContent,
+                    CourseId = item.CourseId,
+                    VideoUrl = item.VideoUrl,
+                    LecturerName = _context.Users.FirstOrDefault(u => u.UserId == _context.Courses.FirstOrDefault(c => c.CourseId == item.CourseId).ExpertId).UserName,
+                    LecturerImage = _context.Users.FirstOrDefault(u => u.UserId == _context.Courses.FirstOrDefault(c => c.CourseId == item.CourseId).ExpertId).Image != null ? _context.Users.FirstOrDefault(u => u.UserId == _context.Courses.FirstOrDefault(c => c.CourseId == item.CourseId).ExpertId).Image : "images/pic-1.jpg",
+                    CreateAt = _context.Courses.FirstOrDefault(c => c.CourseId == item.CourseId).CreateAt.Value.ToString("dd/MM/yyyy")
+                });
+            }
+
+            return Ok(lessonList.AsQueryable());
+
         }
     }
 }
---
+++ Code_Assignment_Project/OnlineLearning_API/DTO/Lesson/LessonDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace OnlineLearning_API.DTO.Lesson
+{
+    public class LessonDTO
+    {
+        public int LessonId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public string? LessonName { get; set; }
+
+        public string? LessonContent { get; set; }
+
+        public string? VideoUrl { get; set; }
+
+        public string? LecturerName { get; set; }
+
+        public string? LecturerImage { get; set; }
+
+        public string? CreateAt { get; set; }
+    }
+}
